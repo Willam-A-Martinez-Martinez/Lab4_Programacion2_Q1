52b6bcff22bdcaa6b8045af314ec00b672692b1a
William A. Martinez Martinez <william_martinez@unitec.edu>
2025-02-14 15:10:13 -0600

    Commit actualizando parametros

---
+++ src/ahorcadoLab4/Gui.java
@@ -0,0 +1,53 @@
+package ahorcadoLab4;
+
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class Gui {
+    JFrame frame = new JFrame("Ahorcado");
+    JLabel titulo = new JLabel("Ahorcado", SwingConstants.CENTER);
+    JButton jugarFijo = new JButton("Jugar Fijo");
+    JButton jugarAzar = new JButton("Jugar al Azar");
+    JButton admin = new JButton("Administrar");
+    JButton salirProgram = new JButton("Salir");
+
+    public Gui() {
+        frame.setLayout(null);
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+
+        
+        titulo.setBounds(150, 50, 200, 50);
+        titulo.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 24));
+
+        
+        jugarFijo.setBounds(150, 150, 200, 50);
+        jugarAzar.setBounds(150, 220, 200, 50);
+        admin.setBounds(150, 290, 200, 50);
+        salirProgram.setBounds(150, 360, 200, 50);
+
+        
+        frame.add(titulo);
+        frame.add(jugarFijo);
+        frame.add(jugarAzar);
+        frame.add(admin);
+        frame.add(salirProgram);
+
+        frame.setVisible(true);
+        
+        admin.addActionListener((ActionEvent e) -> {
+            
+        });
+        salirProgram.addActionListener((ActionEvent e) -> {
+            System.exit(0);
+        });
+    }
+
+    public static void main(String[] args) {
+        new Gui();
+    }
+}
